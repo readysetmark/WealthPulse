296d56adb41e800c6e7dee5bd7231e3fb6e428d7
Mark Williams <mark@readysetmark.com>
2015-03-21 21:30:28 -0500

    Created test file for journal tests

--- JournalTest/ParserPostProcess.fs
+++ JournalTest/ParserPostProcess.fs
@@ -7,39 +7,22 @@ open Journal.Parser.PostProcess
 open Journal.Parser.Types
 open Journal.Types
 
-let sampleFile =
-    [
-        "; first comment";
-        "";
-        "2015-03-20 * Basic transaction ;comment";
-        "  Expenses:Groceries    $45.00";
-        "  Liabilities:Credit";
-        "";
-        "2015-03-20 * Buy stocks";
-        "  Assets:Investments:Stocks    33.245 \"MUTF514\"";
-        "  Assets:Savings               $-250.00";
-        "  Basis:MUTF514:2015-03-20     -33.245 \"MUTF514\"";
-        "  Basis:MUTF514:2015-03-20     $250.00";
-        "";
-        "P 2015-03-20 \"MUTF514\" $7.52";
-        "";
-    ] |> String.concat "\r\n"
+let testFile = "JournalTest/testfiles/simple.dat"
 
-
-let parse parser text =
-    match run parser text with
-    | Success(result, _, _) -> Some(result)
-    | Failure(error, _, _)  -> failwith error
+let parseFile parser filepath =
+    match runParserOnFile parser () filepath System.Text.Encoding.UTF8 with
+    | Success(result, _, _) -> result
+    | Failure(error, _, _) -> failwith error
 
 [<Tests>]
 let extractPricesTests =
     testList "extractPrices" [
-        testCase "all prices extract" <| fun _ ->
+        testCase "all prices extracted" <| fun _ ->
             let symbolPriceDB =
-                parse journal sampleFile
-                |> Option.get
+                parseFile journal testFile
                 |> extractPrices
 
-            Assert.Equal("1 symbol in map", 1, symbolPriceDB.Count)
-            Assert.Equal("1 price for MUTF514", 1, List.length symbolPriceDB.["MUTF514"].Prices)
+            Assert.Equal("2 symbols in map", 2, symbolPriceDB.Count)
+            Assert.Equal("1 price for SII", 1, List.length symbolPriceDB.["SII"].Prices)
+            Assert.Equal("2 prices for WE", 2, List.length symbolPriceDB.["WE"].Prices)
     ]
